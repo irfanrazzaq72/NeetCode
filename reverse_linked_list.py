e380307ccd17a45767f0b76abd9ea7579671890f
irfanrazzaq72 <79426954+irfanrazzaq72@users.noreply.github.com>
2025-05-08 01:35:15 +0200

    Create reverse_linked_list.py

---
+++ reverse_linked_list.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+first_node = Node(30)
+second_node = Node(40)
+third_node = Node(50)
+fourth_node = Node(60)
+
+first_node.next = second_node
+second_node.next = third_node
+third_node.next = fourth_node
+
+head = first_node
+current = head
+while current is not None:
+    print(current.data, end="->")
+    current = current.next
+
+print("None")
+
+
+def reverse_linked_list(head):
+    current_node = head
+    previous = None
+    while current_node:
+        next_node = current_node.next
+        current_node.next = previous
+        previous = current_node
+        current_node = next_node
+
+    head = previous
+    current_node = head
+    while current_node is not None:
+        print(current_node.data, end="->")
+        current_node = current_node.next
+    print("None")
+
+
+reverse_linked_list(head)
